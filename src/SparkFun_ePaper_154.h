5dcd2569d98c1c642dfddba285ab2ed01bfc58dc
Ciara Jekel <39280626+ciarajekel@users.noreply.github.com>
2018-08-13 14:20:02 -0600

    Update SparkFun_ePaper_154.h

--- src/SparkFun_ePaper_154.h
+++ src/SparkFun_ePaper_154.h
@@ -1,3 +1,14 @@
+/*
+  This is a library for the ePaper Display with SRAM and micro SD
+  By: Ciara Jekel
+  SparkFun Electronics
+  Date: August 13th, 2018
+ 
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+ 
+  Feel like supporting our work? Buy a board from SparkFun!
+  https://www.sparkfun.com/products/14892
+*/
 #ifndef SPARKFUN_EPAPER_154_H
 #define SPARKFUN_EPAPER_154_H
 #include <arduino.h>
